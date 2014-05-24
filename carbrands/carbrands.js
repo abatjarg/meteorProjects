0fab0290e4d43e770bd54540f98c35ace4f40079
abatjarg <abatjarg@abatjarg.(none)>
2014-05-24 13:57:21 -0400

    Finished carbrands project

--- carbrands/carbrands.js
+++ carbrands/carbrands.js
@@ -1,19 +1,52 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to carbrands.";
-  };
+var clearValues = function(){
+  $('#make').val("").focus();
+  $('#model').val("");
+  $('#brand').val("");
+}
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+var okCancelEvents = function (selector, callbacks) {
+  var ok = callbacks.ok || function () {};
+  var cancel = callbacks.cancel || function () {};
+
+  var events = {};
+  events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
+    function (evt) {
+      if (evt.type === "keydown" && evt.which === 27) {
+        // escape = cancel
+        cancel.call(this, evt);
+
+      } else if (evt.type === "keyup" && evt.which === 13) {
+        // blur/return/enter = ok/submit if non-empty
+        var value = String(evt.target.value || "");
+        if (value)
+          ok.call(this, value, evt);
+        else
+          cancel.call(this, evt);
+      }
+    };
+
+  return events;
+};
+
+CarBrands = new Meteor.Collection('carbrands');
+
+if (Meteor.isClient) {
+  Template.main.carbrands = function(){
+    return CarBrands.find();
+  }
+
+  Template.main.events(okCancelEvents(
+    '#brand',
+    {
+      ok : function(text, evt){
+        var make = $('#make').val();
+        var model = $('#model').val();
+        CarBrands.insert({make: make, model: model, brand: text});
+        clearValues();
+      }
+    }));
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  
 }
