2c1dde112de435c3c174b76b76e0ac923b599392
abatjarg <abatjarg@abatjarg.(none)>
2014-05-25 22:39:12 -0400

    Started calendar app

--- calendar/client/main.js
+++ calendar/client/main.js
@@ -1,31 +1,37 @@
 CalEvents = new Meteor.Collection('calevents');
 Session.setDefault('editing_calevent', null);
 Session.setDefault('showEditEvent', false);
-Meteor.Router.add({
-  '/' : 'homepage',
-  '/calendar': 'calendar'
-})
+Session.setDefault('lastMod', null);
+Router.map(function(){
+  this.route('homepage', { path: '/'});
+  this.route('calendar', { path: '/calendar'});
+});
 
 Template.calendar.rendered = function(){
   $('#calendar').fullCalendar({
     dayClick:function(date, allDay, jsEvent, view){
-
+      CalEvents.insert({title: 'New event', start: date, end: date});
+      Session.set('lastMod', new Date());
     },
     eventClick:function(calEvent, jsEvent, view){
 
     },
     events:function(start, end, callback){
       var events = [];
-      callEvents = CalEvents.find();
-      calEvent.forEach(function(evt){
+      calEvents = CalEvents.find();
+      calEvents.forEach(function(evt){
         events.push({
           id:evt._id,
           title: evt.title,
           start: evt.start,
           end: evt.end
-        })
+        });
       })
       callback(events);
-    },
+    }
   });
+}
+
+Template.calendar.lastMod = function(){
+  return Session.get('lastMod');
 }
